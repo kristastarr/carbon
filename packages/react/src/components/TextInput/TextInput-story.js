314b602b3299b9c99e43800a3310394a446dde88
Krista.Starr <krista.starr@ibm.com>
2021-10-28 09:15:15 -0700

    add animation ten

--- packages/react/src/components/TextInput/TextInput-story.js
+++ packages/react/src/components/TextInput/TextInput-story.js
@@ -445,3 +445,24 @@ Animation9.parameters = {
   },
 };
 
+export const AnimationZ = () => (
+  <div className={'input-animation-ten'}>
+    <TextInput
+      type={select('Form control type (type)', types, 'text')}
+      {...props.SharedInputProps()}
+      {...props.TextInputProps()}
+    />
+  </div>
+);
+
+AnimationZ.storyName = '10: Underline and Icon shake';
+AnimationZ.parameters = {
+  info: {
+    text: `
+        Text fields enable the user to interact with and input data. A single line
+        field is used when the input anticipated by the user is a single line of
+        text as opposed to a paragraph.
+        The default type is 'text' and its value can be either 'string' or 'number'.
+      `,
+  },
+};
